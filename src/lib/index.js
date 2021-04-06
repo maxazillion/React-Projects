3d0b04f152142c60fdf17b2b3c47d448371e63db
maxazillion <47362990+maxazillion@users.noreply.github.com>
2021-04-05 21:19:22 -0400

    different size cubes

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-function GenerateCubes(number) {
+function generateCubes(number) {
   const colors = [
     "DarkSky w-8 h-8 bg-black border-2 border-black p-1 flex flex-col",
     "Charcoal w-8 h-8 bg-black border-2 border-black p-1 flex flex-col",
@@ -15,4 +15,48 @@ function GenerateCubes(number) {
   return ret;
 }
 
-export { GenerateCubes };
+function makeCubeDiv(cubes) {
+  let startArr = [];
+  let ret = [];
+  let between = [];
+  const root = Math.sqrt(cubes.length);
+
+  cubes.map((cube, index) => {
+    startArr.push(
+      <div key={index} className={cube.color}>
+        {cube.index + 1}
+      </div>
+    );
+  });
+
+  startArr.forEach((element, index) => {
+    if (index % root === 3) {
+      between.push(element);
+      ret.push(makeCubeCols(between, index));
+      between = [];
+    } else {
+      between.push(element);
+    }
+  });
+  console.log(ret);
+
+  return ret.map((cols) => {
+    return cols;
+  });
+}
+
+function makeCubeCols(cubes, key) {
+  return (
+    <div key={key} className="flex gap-2 m-2">
+      {cubes.map((cube) => {
+        return cube;
+      })}
+    </div>
+  );
+}
+
+function makeCubeBlock(cubes) {
+  return makeCubeDiv(cubes);
+}
+
+export { generateCubes, makeCubeBlock };
--- src/pages/CubeTurn/index.js
+++ src/pages/CubeTurn/index.js
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Counter } from "../../components";
-import { GenerateCubes } from "../../lib";
+import { generateCubes, makeCubeBlock } from "../../lib";
 
 function CubeTurn() {
   const [size, setSize] = useState(4);
-  const [cubes, setCubes] = useState(GenerateCubes(size));
+  const [cubes, setCubes] = useState(generateCubes(size));
   const [cubeCounter, setCubeCounter] = useState(0);
 
-  console.log(size);
+  useEffect(() => {
+    setCubes(generateCubes(size));
+    console.log(size);
+  }, [size]);
+
   return (
     <div className="fullScreen shadow-lg min-h-screen flex flex-col p-2 QueenBlue m-2 rounded-xl">
       <h1 className="self-center p-2 text-4xl">Cuboid</h1>
-      <Counter size={size} setSize={setSize} max={8} />
+      <Counter size={size} setSize={setSize} max={8} setCubes={setCubes} />
       <div className="self-center m-2">
         <button className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
           <span className="m-auto text-2xl font-thin">&larr;</span>
@@ -20,15 +24,8 @@ function CubeTurn() {
           <span className="m-auto text-2xl font-thin">&rarr;</span>
         </button>
       </div>
-      <div className="flex gap-2">
-        {cubes.map((cube, index) => {
-          return (
-            <div key={index} className={cube.color}>
-              <h1 className="text-white self-center">{cube.index + 1}</h1>
-            </div>
-          );
-        })}
-      </div>
+      <div className="self-center">{makeCubeBlock(cubes)}</div>
+      <div className="flex gap-2"></div>
     </div>
   );
 }
